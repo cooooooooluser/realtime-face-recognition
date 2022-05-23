69fb666c698c2ec18fa2ac6573cb08e8a0e37514
cooooooooluser <85533967+cooooooooluser@users.noreply.github.com>
2022-05-22 18:20:34 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,32 @@
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO)
+    video.hide();
+    imageClassifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/H5WodaQ8-/model.json",modelLoaded);
+
 }
 
 function draw() {
-
+    image(video, 0, 0, 300,300);
+    console.log("finished");
+   imageClassifier.classify(video, gotResult);
 }
+
+function modelLoaded() {
+   console.log("modelLoaded")
+}
+
+function gotResult(error, results) {
+   if (error){
+      console.error(error);
+   }
+   else{
+      console.log(results);
+      document.getElementById("object_value").innerHTML=results[0].label;
+      document.getElementById("Acurracy_number").innerHTML=results[0].confidence.toFixed(3);
+   }
+}
+
+//image link :
+//"https://teachablemachine.withgoogle.com/models/H5WodaQ8-/model.json"
